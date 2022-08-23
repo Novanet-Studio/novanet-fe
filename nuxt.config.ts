a96b0a6e8048aad2344366d5d5601cdc8ed25049
Fredy Polania <fpolania@gmail.com>
2022-08-22 21:32:27 -0400

    add:Strapi config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     ],
   },
   buildModules: ["@nuxtjs/style-resources"],
-  modules: ["@nuxt/image-edge"],
+  modules: ["@nuxt/image-edge", "@nuxtjs/strapi"],
   vite: {
     css: {
       preprocessorOptions: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
   <section class="section section--home">
-    <h1 class="shapes__titulo">Esto es un h1</h1>
+    <h1 class="shapes__titulo">{{ query.data.inicio.principal }}</h1>
   </section>
 </template>
 
-<script>
-export default {};
+<script setup lang="ts">
+const graphql = useStrapiGraphQL();
+// Option 1: use inline query
+const query = await graphql(`
+  query {
+    inicio {
+      principal
+    }
+  }
+`);
 
 import "./index.scss";
 </script>
