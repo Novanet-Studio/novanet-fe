aea81ae48e22827133aa7bbc007d732568b88356
David Arenas <davejs136@gmail.com>
2022-12-19 16:40:12 -0400

    fix: fontawesome config

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="section section--home">
-    <h1 class="shapes__titulo">{{ query.data.inicio.principal }}</h1>
+    <!-- <h1 class="shapes__titulo">{{ query.data.inicio.principal }}</h1> -->
   </section>
 </template>
 
 <script setup lang="ts">
-const graphql = useStrapiGraphQL();
-// Option 1: use inline query
-const query = await graphql(`
-  query {
-    inicio {
-      principal
-    }
-  }
-`);
+// import "./index.scss";
+// const graphql = useStrapiGraphQL();
+// // Option 1: use inline query
+// const query = await graphql(`
+//   query {
+//     inicio {
+//       principal
+//     }
+//   }
+// `);
 
-import "./index.scss";
 </script>
