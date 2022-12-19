4b24ede9701ee06b3b94b5c3378865046187087f
David Arenas <davejs136@gmail.com>
2022-12-19 16:21:47 -0400

    chore: remove sass-loader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-// import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   telemetry: false,
@@ -11,7 +9,6 @@ export default defineNuxtConfig({
       "@fortawesome/free-brands-svg-icons",
     ],
   },
-  // buildModules: ["@nuxtjs/style-resources"],
   modules: ["@nuxt/image-edge", "@nuxtjs/strapi"],
   vite: {
     css: {
