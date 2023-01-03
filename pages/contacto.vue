e62a92bed9b227cc1a2871e702b7fb700b2b2ff0
Fredy Polania <fpolania@gmail.com>
2023-01-03 12:12:09 -0400

    fix: contacto form info

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,60 +1,105 @@
 <template>
   <container-wrapper>
-  <custom-header icon="z" view="Contacto" bg="header--blue" title-class="nav__title" />
-  <main class="main">
-    <section class="section">
-      <div class="contacto">
-        <div class="contacto__items">
-          <h2 class="blue">
-            {{ contact?.data.attributes.principal.titulo }}
-          </h2>
-          <div v-html="markdown.render(contact?.data.attributes.principal.descripcion ?? '')"></div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d980.7857007733666!2d-66.87772169073442!3d10.489406951509675!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1952146d76df33c4!2sNovanet%20Studio!5e0!3m2!1ses!2sve!4v1632839029172!5m2!1ses!2sve"
-            class="contacto__mapa" allowfullscreen="true" loading="lazy"></iframe>
-        </div>
+    <custom-header
+      icon="z"
+      view="Contacto"
+      bg="header--blue"
+      title-class="nav__title"
+    />
+    <main class="main">
+      <section class="section">
+        <div class="contacto">
+          <div class="contacto__items">
+            <h2 class="blue">
+              {{ contact?.data.attributes.principal.titulo }}
+            </h2>
+            <div
+              v-html="
+                markdown.render(
+                  contact?.data.attributes.principal.descripcion ?? '',
+                )
+              "
+            ></div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d980.7857007733666!2d-66.87772169073442!3d10.489406951509675!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1952146d76df33c4!2sNovanet%20Studio!5e0!3m2!1ses!2sve!4v1632839029172!5m2!1ses!2sve"
+              class="contacto__mapa"
+              allowfullscreen="true"
+              loading="lazy"
+            ></iframe>
+          </div>
 
-        <div class="contacto__items">
-          <h2 class="blue">
-            {{ contact?.data.attributes.formulario.titulo }}
-          </h2>
-          <p>
-            {{ contact?.data.attributes.formulario.descripcion }}
-          </p>
-          <form class="formulario" name="contacto" @submit.prevent="onSubmit" action="/gracias/" method="post"
-            data-netlify="true" data-netlify-honeypot="bot-field">
-            <h3 class="formulario__titulo">Formulario de contacto</h3>
-
-            <p hidden>
-              <label> Don’t fill this out: <input name="bot-field" /> </label>
+          <div class="contacto__items">
+            <h2 class="blue">
+              {{ contact?.data.attributes.formulario.titulo }}
+            </h2>
+            <p>
+              {{ contact?.data.attributes.formulario.descripcion }}
             </p>
-            <div>
-              <input type="text" name="nombre" v-model="name" placeholder="Nombre y Apellido" />
-            </div>
+            <form
+              class="formulario"
+              name="contacto"
+              @submit.prevent="onSubmit"
+              action="/gracias/"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <h3 class="formulario__titulo">Formulario de contacto</h3>
 
-            <div>
-              <input type="email" name="email" v-model="email" placeholder="Email" />
-            </div>
+              <p hidden>
+                <label> Don’t fill this out: <input name="bot-field" /> </label>
+              </p>
+              <div>
+                <input
+                  type="text"
+                  name="nombre"
+                  v-model="name"
+                  placeholder="Nombre y Apellido"
+                />
+              </div>
 
-            <div>
-              <input type="number" name="telefono" v-model="phone" placeholder="Teléfono" />
-            </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  v-model="email"
+                  placeholder="Email"
+                />
+              </div>
 
-            <div>
-              <input type="text" name="origen" v-model="origin" placeholder="¿Cómo nos encontró?" />
-            </div>
+              <div>
+                <input
+                  type="number"
+                  name="telefono"
+                  v-model="phone"
+                  placeholder="Teléfono"
+                />
+              </div>
 
-            <div>
-              <textarea name="mensaje" v-model="message" placeholder="Mensaje" />
-            </div>
-            <input type="submit" value="Enviar" />
-            <input type="hidden" name="form-name" value="inicio" />
-          </form>
+              <div>
+                <input
+                  type="text"
+                  name="origen"
+                  v-model="origin"
+                  placeholder="¿Cómo nos encontró?"
+                />
+              </div>
+
+              <div>
+                <textarea
+                  name="mensaje"
+                  v-model="message"
+                  placeholder="Mensaje"
+                />
+              </div>
+              <input type="submit" value="Enviar" />
+              <input type="hidden" name="form-name" value="inicio" />
+            </form>
+          </div>
         </div>
-      </div>
-    </section>
-  </main>
-</container-wrapper>
+      </section>
+    </main>
+  </container-wrapper>
 </template>
 
 <script lang="ts" setup>
@@ -66,6 +111,10 @@ definePageMeta({
   layout: 'page',
 });
 
+const clog = (e: any) => {
+  console.log(e);
+};
+
 const graphql = useStrapiGraphQL();
 const contact = ref<Project.Contact>();
 const markdown = new MarkdownIt();
@@ -75,7 +124,10 @@ useHead({
   meta: [
     {
       name: 'description',
-      content: contact.value?.data.attributes.principal.descripcion.substring(0, 168),
+      content: contact.value?.data.attributes.principal.descripcion.substring(
+        0,
+        168,
+      ),
     },
   ],
 });
@@ -89,17 +141,21 @@ const schema = yup.object({
 });
 
 // We can show error message if exist
-const { useFieldModel, errors: _, handleSubmit } = useForm({
+const {
+  useFieldModel,
+  errors: _,
+  handleSubmit,
+} = useForm({
   validationSchema: schema,
 });
 
-const [
-  name,
-  email,
-  phone,
-  origin,
-  message,
-] = useFieldModel(['name', 'email', 'phone', 'origin', 'message']);
+const [name, email, phone, origin, message] = useFieldModel([
+  'name',
+  'email',
+  'phone',
+  'origin',
+  'message',
+]);
 
 const router = useRouter();
 
@@ -122,7 +178,7 @@ const onSubmit = handleSubmit(async (values) => {
   } catch (error) {
     console.log('An error occurred while sending the form data: ', error);
   }
-})
+});
 
 try {
   const response = await graphql<Project.ContactResponse>(`
