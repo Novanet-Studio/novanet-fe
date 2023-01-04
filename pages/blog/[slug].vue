e9024af430ddf997a97e7998bee4e2f3e2c8e202
Dave136 <davejs136@gmail.com>
2023-01-04 15:03:45 -0400

    fix: render article correclty

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,22 +1,44 @@
 <template>
   <container-wrapper>
-    <custom-header icon="q" :view="`Blog / ${articleItem?.attributes.titulo}`" bg="header--green"
-      title-class="nav__link--title" />
+    <custom-header
+      icon="q"
+      :view="`Blog / ${articleItem?.attributes?.titulo}`"
+      bg="header--green"
+      title-class="nav__link--title"
+    />
     <main class="main">
       <section class="section">
         <article class="article">
           <div class="article__image-share">
-            <template v-if="articleItem?.attributes.imagen.data[0].attributes.url">
-              <nuxt-img class="article__image" :src="articleItem?.attributes.imagen.data[0].attributes.url"
-                :alt="articleItem?.attributes.imagen.data[0].attributes.alternativeText" background="#ededed" />
+            <template
+              v-if="articleItem?.attributes.imagen.data[0].attributes.url"
+            >
+              <nuxt-img
+                class="article__image"
+                :src="articleItem?.attributes.imagen.data[0].attributes.url"
+                :alt="
+                  articleItem?.attributes.imagen.data[0].attributes
+                    .alternativeText
+                "
+                background="#ededed"
+              />
             </template>
-            <app-share class="article__share" :url="currentUrl" :titulo="articleItem!.attributes.titulo"
+            <app-share
+              class="article__share"
+              :url="currentUrl"
+              :titulo="articleItem!.attributes.titulo"
               :descripcion="articleItem!.attributes.descripcion"
-              :imagen="articleItem!.attributes.imagen.data[0].attributes.url" />
+              :imagen="articleItem!.attributes.imagen.data[0].attributes.url"
+              v-if="articleItem?.attributes?.titulo"
+            />
           </div>
           <div class="article__info">
             <h1 class="article__title">{{ articleItem?.attributes.titulo }}</h1>
-            <div v-html="markdown.render(articleItem?.attributes.descripcion ?? '')" />
+            <div
+              v-html="
+                markdown.render(articleItem?.attributes.descripcion ?? '')
+              "
+            />
           </div>
         </article>
       </section>
@@ -32,7 +54,7 @@ definePageMeta({
 });
 
 const graphql = useStrapiGraphQL();
-const articleId = useArticleId();
+const route = useRoute();
 const currentUrl = useCurrentUrl();
 const markdown = new MarkdownIt();
 
@@ -40,40 +62,46 @@ const articleItem = ref<Project.Article>();
 
 useHead({
   title: articleItem.value?.attributes.titulo,
-  meta: [{
-    name: 'description',
-    content: articleItem?.value?.attributes.descripcion.substring(0, 158),
-  }]
-})
+  meta: [
+    {
+      name: 'description',
+      content: articleItem?.value?.attributes.descripcion.substring(0, 158),
+    },
+  ],
+});
 
 try {
-  const response = await graphql<Project.ArticleResponse>(`
-    query Articles($id: ID!) {
-      articulo(id: $id) {
-        data {
-          id
-          attributes {
-            titulo
-            descripcion
-            tag
-            fecha
-            imagen {
-              data {
-                attributes {
-                  url
-                  alternativeText
+  const response = await graphql<Project.ArticlesResponse>(
+    `
+      query Articles($slug: String!) {
+        articulos(filters: { slug: { eq: $slug } }) {
+          data {
+            id
+            attributes {
+              titulo
+              descripcion
+              tag
+              fecha
+              imagen {
+                data {
+                  attributes {
+                    url
+                    alternativeText
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  `, {
-    id: articleId.value,
-  });
+    `,
+    {
+      slug: route.params.slug,
+    },
+  );
 
-  articleItem.value = response.data.articulo.data as Project.Article;
+  const data = response.data.articulos.data as Project.Article[];
+  articleItem.value = data[0];
 } catch (error) {
   console.error('An error ocurred while fetching article data: ', error);
 }
