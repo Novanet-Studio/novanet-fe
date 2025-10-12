120b500d268c7b61b4850a3d884a5075afe2bbfd
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-10-12 19:12:37 -0400

    add: main hero section in blog.vue

---
+++ app/pages/blog/[tag]/[slug].vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+useHead({
+  title: "Blog | Novanet Studio",
+});
+</script>
+
+<template>
+  <div class="section__container">asd</div>
+</template>
---
+++ app/pages/blog/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+useHead({
+  title: "Blog | Novanet Studio",
+});
+
+const blog = [
+  {
+    name: "Blog",
+
+    title: "Creatividad y conocimiento",
+    titleColor: "text-oxfordBlue",
+    description:
+      "Exploramos temas de diseño gráfico, consejos sobre desarrollo web y asesoría personalizada para ayudarte a alcanzar sus metas digitales.",
+    color: "text-oxfordBlue",
+
+    dataColor: "azure",
+
+    bgColor: "bg-azure",
+
+    HeroImage: "images/novanet-studio-modelo-telefono.webp",
+  },
+];
+</script>
+
+<template>
+  <div class="section__container">
+    <CommonSection :content="blog" />
+  </div>
+</template>
