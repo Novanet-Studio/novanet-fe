3f9fde3c46051aef7a02f2b6a1958ef46c9fa9ac
Fredy Polania <fpolania@gmail.com>
2025-11-14 13:28:54 -0400

    fix: colorMap value

--- app/utils/colorMap.ts
+++ app/utils/colorMap.ts
@@ -4,7 +4,7 @@ export const colorMap: Record<string, string> = {
   oxfordBlue: "#CCE5FF",
   columbiaBlue: "#001933",
   white: "#001933",
-  chartreuse: "#007FFF",
+  chartreuse: "#D6F630",
   raspberry: "#DF1F5B",
   // add more as needed
 };
